c76d6c1770a95cd9249789a02934d742cc1495e6
Craig <craigmarroquin@CSU.fullerton.edu>
2016-02-27 03:21:09 -0800

    Fixed the redirect for Create Event and Log out.

--- source/production/java/com/Calendar/UserHomeServlet.java
+++ source/production/java/com/Calendar/UserHomeServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -15,7 +16,8 @@ import java.util.Map;
 /**
  * Created by BHARATH on 2/26/2016.
  */
-@WebServlet(name = "UserHomeServlet", urlPatterns = {"/loginsuccess","/welcome"})
+@WebServlet(name = "UserHomeServlet",
+        urlPatterns = {"/loginsuccess","/welcome"})
 public class UserHomeServlet extends HttpServlet {
 
     private Map<Integer, event> eventDatabase = new LinkedHashMap<>();
@@ -23,7 +25,7 @@ public class UserHomeServlet extends HttpServlet {
 
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -45,33 +47,60 @@ public class UserHomeServlet extends HttpServlet {
                 this.userHome(request, response);
                 break;
             case "logout":
-                this.userHome(request, response);
+                this.logout(request, response);
                 break;
             default:
                 this.userHome(request, response);
                 break;
         }
     }
-        private void createEvent(HttpServletRequest request,HttpServletResponse response)
+
+    /*********************************************************
+     * CreateEvent
+     * Allow the user to create events
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     ********************************************************/
+    private void createEvent(HttpServletRequest request,HttpServletResponse response)
         throws ServletException, IOException
         {
             request.getRequestDispatcher("/WEB-INF/jsp/view/createEvent.jsp")//to create an event
                     .forward(request, response);
         }
-          private void userHome(HttpServletRequest request,HttpServletResponse response)
+    /*********************************************************
+     *userHome
+     * Redirect to userHome page
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     ********************************************************/
+    private void userHome(HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException
          {
         request.getRequestDispatcher("/WEB-INF/jsp/view/welcome.jsp")//User's Home page
                 .forward(request, response);
         }
+    /*********************************************************
+     *logout
+     * Logs the user out
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     ********************************************************/
     private void logout(HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException
     {
-
-        System.out.println("In log out");//to invalidate the session
+        HttpSession session = request.getSession();
+        session.invalidate();//to invalidate the session
+        request.getRequestDispatcher("/WEB-INF/jsp/view/logout.jsp")
+                .forward(request, response);
     }
 
 
 
 
-    }
+}
--- web/WEB-INF/jsp/view/logout.jsp
+++ web/WEB-INF/jsp/view/logout.jsp
@@ -1,17 +1,17 @@
 <%--
   Created by IntelliJ IDEA.
   User: craig
-  Date: 2/22/16
-  Time: 7:38 PM
+  Date: 2/27/16
+  Time: 3:01 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Registration Successful</title>
+    <title>Logout</title>
 </head>
 <body>
-    <h2>You have successfully registered!</h2>
-    <a href="?">Click here to be redirected</a>
+    <h2>Successfully logged out</h2>
+    <h4>Come back soon!</h4>
 </body>
 </html>
